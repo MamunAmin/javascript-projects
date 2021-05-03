84c0337ffd0c38c2382e6790e22c1b2553baf89d
MamunAmin <mamun.amin2020@gmail.com>
2021-05-03 22:36:32 +0600

    changed

---
+++ quiz-app/script.js
@@ -0,0 +1,34 @@
+const quizData = [
+    {
+        question: "Who is the Billionaire?",
+        a: "Hero Alom",
+        b: "Obaidul Kader",
+        c: "Elon Mask",
+        d: "Abul Hayat",
+        answer: "c"
+    },
+    {
+        question: "Which of the following is a soft drink?",
+        a: "Amazon",
+        b: "Uniliver",
+        c: "Cocacola",
+        d: "Durex",
+        answer: "c"
+    },
+    {
+        question: "Who is Isaac Newton?",
+        a: "A Scientist",
+        b: "A Singer",
+        c: "A Terrorist",
+        d: "An Icecream Seller",
+        answer: "a"
+    },
+    {
+        question: "What does programmer do?",
+        a: "Drink coffee",
+        b: "Write Code",
+        c: "Dance like a popstar",
+        d: "Eat Soil",
+        answer: "b"
+    }
+]
